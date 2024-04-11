7f76837f9c55f5037707e74455d4fd6656359ccf
Joel Clarke <joel.clarke273@gmail.com>
2024-04-11 19:07:46 +0100

    Create Footer Component

---
+++ src/components/footer.tsx
@@ -0,0 +1,16 @@
+import NavButton from "@/components/navbutton";
+
+
+export default function Footer() {
+  return (
+    <footer className="flex flex-col items-center p-12 w-full space-y-5">
+      <ul className="flex divide-x-2">
+        <NavButton text="Email" url="mailto:joel.clarke273@gmail.com"/>
+        <NavButton text="GitHub" url="https://github.com/DarthEmpty"/>
+        <NavButton text="LinkedIn" url="https://www.linkedin.com/in/joel-clarke-7baa16128/"/>
+      </ul>
+
+      <p className="text-sm">Made with <strong className="text-accent">NextJS</strong>, using <strong className="text-accent">TypeScript</strong> and <strong className="text-accent">TailwindCSS</strong></p>
+    </footer>
+  )
+}
