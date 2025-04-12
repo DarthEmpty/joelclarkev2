c52f06ab175b79fde123566ed446dc280457d154
DarthEmpty <joel.clarke273@gmail.com>
2025-04-12 23:08:15 +0100

    Move CV Nav button to Homepage center

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
-import Typewriter from "@/components/typewriter";
 import Image from "next/image";
+import Link from "next/link";
+import Typewriter from "@/components/typewriter";
 
 const FACE_IMAGE_PATH = "/images/face.jpg";
+const CV_PATH = "/SoftEngCV.pdf";
+const ROLES = [
+  "Software Engineer",
+  "IT Technician",
+  "Writer",
+  "Artist",
+  "Actor",
+  "Improv Comedian",
+  "Runner",
+  "Musician",
+];
 
 const Home = () => (
   <main className="flex flex-col mx-auto h-screen">
@@ -15,22 +27,20 @@ const Home = () => (
         className="object-cover size-64 rounded-3xl mx-auto my-7 md:mx-0 md:my-0"
       />
 
-      <div className="w-64 mx-auto md:w-full md:mx-0">
+      <div className="w-64 mx-auto flex flex-col md:w-full md:mx-0">
         <Typewriter
-          texts={[
-            "Software Engineer",
-            "IT Technician",
-            "Writer",
-            "Artist",
-            "Actor",
-            "Improv Comedian",
-            "Runner",
-            "Musician",
-          ]}
+          texts={ROLES}
           className="text-xl font-semibold text-accent-light md:text-3xl"
         />
         <p className="md:text-lg">Hello - I&apos;m Joel!</p>
         <p className="md:text-lg">Welcome to my corner of the internet.</p>
+        <div className="md:grow" />
+        <Link
+          className="mt-4 p-1 text-center bg-accent-dark active:bg-accent-light md:hover:bg-accent-light md:active:bg-accent-dark rounded-3xl md:mr-36"
+          href={CV_PATH}
+        >
+          Check out my CV!
+        </Link>
       </div>
     </div>
     <div className="md:grow" />
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,61 +1,7 @@
-"use client";
-
-import { Hamburger, LeftArrow } from "@/components/icons";
-import NavButton from "@/components/navbutton";
-import { MouseEventHandler, useState } from "react";
-
-interface NavBarButtonGroupProps {
-  className?: string;
-  onClick?: MouseEventHandler;
-}
-
-const NavBarButtonGroup = (props: NavBarButtonGroupProps) => (
-  <ul className={props.className}>
-    <NavButton text="Home" url="/" onClick={props.onClick} />
-    <NavButton text="CV" url="/SoftEngCV.pdf" onClick={props.onClick} />
-    {/* COMING SOON! */}
-    {/* <NavButton text="Projects" url="/#" onClick={props.onClick} />
-    <NavButton text="Gallery" url="/#" onClick={props.onClick} /> */}
-  </ul>
+const NavBar = () => (
+  <nav className="sticky top-0 flex w-full items-center bg-primary-light p-7 dark:bg-primary-dark md:fixed">
+    <h1 className="text-4xl font-bold uppercase tracking-wide">Joel Clarke</h1>
+  </nav>
 );
-
-function NavBar() {
-  const [showNavDrawer, setShowNavDrawer] = useState(false);
-
-  return (
-    <nav className="sticky top-0 flex w-full items-center bg-primary-light p-7 dark:bg-primary-dark md:fixed">
-      <button
-        className="mr-5 rounded-full bg-accent-light p-2 md:collapse dark:bg-accent-dark"
-        onClick={() => setShowNavDrawer(true)}
-      >
-        <Hamburger />
-      </button>
-
-      <h1 className="text-4xl font-bold uppercase tracking-wide">
-        Joel Clarke
-      </h1>
-
-      <div className="shrink md:grow" />
-
-      <NavBarButtonGroup className="invisible shrink md:visible md:flex md:text-lg" />
-
-      {showNavDrawer && (
-        <div className="absolute left-0 top-0 flex h-screen w-2/3 flex-col items-center bg-white md:invisible dark:bg-black">
-          <NavBarButtonGroup
-            className="m-10 space-y-5 text-4xl"
-            onClick={() => setShowNavDrawer(false)}
-          />
-
-          <button
-            className="mt-5 rounded-full bg-accent-light p-2 md:collapse dark:bg-accent-dark"
-            onClick={() => setShowNavDrawer(false)}
-          >
-            <LeftArrow />
-          </button>
-        </div>
-      )}
-    </nav>
-  );
-}
 
 export default NavBar;
